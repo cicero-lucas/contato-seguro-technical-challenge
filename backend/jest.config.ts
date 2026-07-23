aa0af849f58e28063b6a02710f9fb4f4a6b94e09
cicero lucas <cicerolucassilva10@gmail.com>
2026-07-23 16:16:56 -0300

    chore(backend): fix tsconfig rootDir conflict and add tsconfig.test.json for Jest

--- backend/jest.config.ts
+++ backend/jest.config.ts
@@ -6,7 +6,7 @@ const config: Config = {
   roots: ["<rootDir>/tests"],
   testMatch: ["**/*.test.ts"],
   transform: {
-    "^.+\\.tsx?$": ["ts-jest", { tsconfig: "tsconfig.json" }],
+    "^.+\\.tsx?$": ["ts-jest", { tsconfig: "tsconfig.test.json" }],
   },
   moduleNameMapper: {
     "^@/(.*)$": "<rootDir>/src/$1",
